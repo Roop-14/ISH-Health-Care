594e4835d9d3082000ee531ad31da70655a93e66
Roopam <roopammondal14@gmail.com>
2025-06-28 20:14:30 +0530

    jar to war

--- SSA-WEB-API/src/main/java/com/ssaapi/SsaWebApiApplication.java
+++ SSA-WEB-API/src/main/java/com/ssaapi/SsaWebApiApplication.java
@@ -2,9 +2,16 @@ package com.ssaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SsaWebApiApplication {
+public class SsaWebApiApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// Register your application class
+		return builder.sources(SsaWebApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsaWebApiApplication.class, args);
